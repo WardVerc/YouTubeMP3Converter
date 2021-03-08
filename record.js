73be3dce492ac29c32d80c6006f67a8e04630a2a
Ward Vercruyssen <r0435477@student.vives.be>
2021-03-08 19:12:50 +0100

    Enkel layout nog geen functionaliteit

---
+++ record.js
@@ -0,0 +1,27 @@
+//let's build a YouTube to mp3 converter
+
+
+//Open the YouTube link provided by the user when the user clicks the convert button
+
+
+//Open Audacity and make sure a new project is opened/it is blank
+
+
+//Make sure that the settings under the control buttons of Audacity are Windows WASAPI
+//and the recording device is your computer's primary output device
+
+
+//hit the record button
+
+
+//press play from 0:00 on the YouTube video
+
+
+//when video is done, stop recording
+
+//close the youtube website
+
+//in audacity, go to File > Export audio.
+
+//give it a name (the name of the video), change the setting to .mp3 and save and click OK
+
